257df1720f72cf52ef93d6b76ac557021ff1d36b
Ashutosh00710 <starcdwivedi@gmail.com>
2021-07-17 13:54:56 +0530

    add: Next Permutation

---
+++ array/NextPermutation.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    void reverse(vector<int>& arr, int start, int end) {
+        while (start < end) swap(arr[start++], arr[end--]);
+    }
+    void nextPermutation(vector<int>& nums) {
+        int n = nums.size();
+        if (n == 1) return;
+        int i = n - 2, j = n - 1;
+        while (i >= 0 && nums[i] >= nums[i + 1]) --i;
+        if (i >= 0) {
+            j = n - 1;
+            while (j >= 0 && nums[i] >= nums[j]) --j;
+            swap(nums[i], nums[j]);
+        }
+        reverse(nums, i + 1, n - 1);
+    }
+
+    void display(vector<int>& nums) {
+      cout << "[";
+      string str = "";
+      for(auto i : nums) {
+        cout <<str<< i;
+        str = ", ";
+      }
+      cout << "]"<<endl;
+    }
+};
+
+int main() {
+  vector<int> nums1{1, 2, 3};
+  vector<int> nums2{3, 2, 1};
+  vector<int> nums3{1, 1, 5};
+  vector<int> nums4{1};
+  Solution s;
+  s.nextPermutation(nums1);
+  s.display(nums1);
+  s.nextPermutation(nums2);
+  s.display(nums2);
+  s.nextPermutation(nums3);
+  s.display(nums3);
+  s.nextPermutation(nums4);
+  s.display(nums4);
+  return 0;
+}
