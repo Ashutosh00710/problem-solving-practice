58fd6feccf7648f47c4c6ff7f4c9eb08c7e2fc19
Ashutosh00710 <starcdwivedi@gmail.com>
2021-07-19 11:13:48 +0530

    update: BestTimeToBuyandSell.cpp

--- array/BestTimeToBuyandSell.cpp
+++ array/BestTimeToBuyandSell.cpp
@@ -14,13 +14,21 @@ public:
         }
         return maxSoFar;
     }
+
+    int maxProfitSecondApproach(vector<int>& prices) {
+        int max = 0;
+        for(int i = 1;  i < prices.size(); i++) {
+            if(prices[i] > prices[i - 1]) max += prices[i] - prices[i - 1];
+        }
+        return max;
+    }
 };
 
 int main() {
     Solution sol;
     vector<int> test1 = {7,1,5,3,6,4};
     vector<int> test2 = {7,6,4,3,1};
-    cout << sol.maxProfit(test1) << endl;
-    cout << sol.maxProfit(test2) << endl;
+    cout << sol.maxProfitSecondApproach(test1) << endl;
+    cout << sol.maxProfitSecondApproach(test2) << endl;
     return 0;
 }
